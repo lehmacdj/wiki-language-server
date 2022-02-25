94dd3f95146bb30469eb6e32aa5c7a7848c1897f
Devin Lehmacher <lehmacdj@gmail.com>
2022-02-25 15:27:24 +0100

    flip note, to make use more ergonomic
    although this function is typically the way I originally wrote it, I
    think it is a little bit easier to use it this way, because then it
    isn't necessary to name errors if they get too long to include before
    the thing that is being noted, whereas naming the result (e.g. mresult)
    is fairly simple

--- src/MyPrelude.hs
+++ src/MyPrelude.hs
@@ -36,15 +36,15 @@ import GHC.TypeLits as X
 -- | Throw an error in place of Nothing. So named because generally the
 -- exception will describe the error that took place causing a result of
 -- Nothing embedding it in a larger class of possible exceptions.
-note :: MonadError e m => e -> Maybe a -> m a
-note = noteM . pure
+note :: MonadError e m => Maybe a -> e -> m a
+note v = noteM v . pure
 
--- | Like noteM, but allows running monadic effects while generating the
--- exception to throw. This is useful in cases when, for example, one wants to
--- do some logging as well (or in the case of an LSP implementation report a
--- diagnostic) as well.
-noteM :: MonadError e m => m e -> Maybe a -> m a
-noteM err = \case
+-- | Like noteM, but flipped and allows running monadic effects while
+-- generating the exception to throw. This is useful in cases when, for
+-- example, one wants to do some logging as well (or in the case of an LSP
+-- implementation report a diagnostic) as well.
+noteM :: MonadError e m => Maybe a -> m e -> m a
+noteM v err = case v of
   Nothing -> throwError =<< err
   Just x -> pure x
 
