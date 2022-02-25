956b218f856e7ce4bde8f00b948fe9cca5c6dfa9
Devin Lehmacher <lehmacdj@gmail.com>
2022-02-25 15:14:23 +0100

    add some utilities to prelude

--- src/MyPrelude.hs
+++ src/MyPrelude.hs
@@ -3,6 +3,9 @@ module MyPrelude
     module ClassyPrelude,
 
     -- * Custom exports, in this module
+    codiagonal,
+    note,
+    noteM,
 
     -- * Various other things we want to export
     module X,
@@ -22,8 +25,30 @@ import Control.Lens as X
     _Nothing,
     _Right,
   )
+import Control.Monad.Error.Class as X (MonadError (..))
+import Control.Monad.Except as X (ExceptT (..), runExceptT)
 import Control.Monad.Reader.Class as X
 import Data.Default as X (Default (..))
 import Data.Generics.Labels ()
 import Data.List.NonEmpty as X (NonEmpty (..))
 import GHC.TypeLits as X
+
+-- | Throw an error in place of Nothing. So named because generally the
+-- exception will describe the error that took place causing a result of
+-- Nothing embedding it in a larger class of possible exceptions.
+note :: MonadError e m => e -> Maybe a -> m a
+note = noteM . pure
+
+-- | Like noteM, but allows running monadic effects while generating the
+-- exception to throw. This is useful in cases when, for example, one wants to
+-- do some logging as well (or in the case of an LSP implementation report a
+-- diagnostic) as well.
+noteM :: MonadError e m => m e -> Maybe a -> m a
+noteM err = \case
+  Nothing -> throwError =<< err
+  Just x -> pure x
+
+codiagonal :: Either a a -> a
+codiagonal = \case
+  Left a -> a
+  Right a -> a
