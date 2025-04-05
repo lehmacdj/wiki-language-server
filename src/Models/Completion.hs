7ee42ec9fb3028f17d78ce1db319a2953dc9788e
Devin Lehmacher <lehmacdj@gmail.com>
2025-04-05 15:31:18 -0400

    Add tests for completion + fix bug

--- src/Models/Completion.hs
+++ src/Models/Completion.hs
@@ -1,6 +1,6 @@
 module Models.Completion where
 
-import Data.Text (breakOn)
+import Data.Text (breakOn, breakOnEnd)
 import Language.LSP.Protocol.Types
 import Models.NoteInfo
 import Models.Slug
@@ -20,23 +20,29 @@ data Completion
   }
   deriving (Show, Eq, Generic)
 
+wikiLinkCompletion :: Range -> Slug -> Text -> Text -> Completion
+wikiLinkCompletion replaceRange slug title typedAlias =
+  WikiLinkCompletion
+    { label = title ++ " (" ++ slug.text ++ ")",
+      ..
+    }
+
 makeWikiLinkCompletionsFromLine :: NoteInfoCache -> Position -> Text -> [Completion]
 makeWikiLinkCompletionsFromLine noteInfos position line = withEarlyReturn_ do
-  let reversed = reverse line
-      (reversedPrefix, reversedRest) = breakOn "[[" reversed
-  when ("]]" `isInfixOf` reversedPrefix) $ returnEarly []
-  let prefix = reverse reversedPrefix
-      (start, end) = breakOn "|" prefix
-      completionPrefix = if null end then start else end
+  let (rest, completionContext) = breakOnEnd "[[" line
+  when ("]]" `isInfixOf` completionContext) $ returnEarly []
+  when (null rest) $ returnEarly [] -- there is no `[[` in the line
+  let (start, end) = breakOn "|" completionContext
+      searchPrefix = if null end then start else end
   pure
-    $ Fuzzy.filter completionPrefix (toList noteInfos) "" "" (.title) False
+    $ Fuzzy.filter searchPrefix (toList noteInfos) "" "" (.title) False
     <&> \(Fuzzy NoteInfo {..} _ _) ->
       WikiLinkCompletion
-        { replaceRange = Range (sameLineWithCol position (length reversedRest)) position,
+        { replaceRange = Range (sameLineWithCol position (length rest)) position,
           slug,
           title,
           label = title ++ " (" ++ slug.text ++ ")",
-          typedAlias = prefix
+          typedAlias = completionContext
         }
 
 renderCompletionForLineNum :: Completion -> [CompletionItem]
@@ -72,3 +78,24 @@ renderCompletionForLineNum WikiLinkCompletion {..} =
           _command = Nothing,
           _data_ = Nothing
         }
+
+spec_makeWikiLinkCompletionsFromLine :: Spec
+spec_makeWikiLinkCompletionsFromLine = do
+  it "returns completions when there is a fuzzy match"
+    $ makeWikiLinkCompletionsFromLine fakeNoteInfoCache (P 0 7) "[[Hello"
+    `shouldBe` [wikiLinkCompletion (Range (P 0 2) (P 0 7)) (Slug "kWp7rk0suUXd") "Hello world" "Hello"]
+  it "doesn't return completions when no [["
+    $ makeWikiLinkCompletionsFromLine fakeNoteInfoCache (P 0 5) "Hello"
+    `shouldBe` []
+  it "returns all matching completions sorted in a reasonable order"
+    $ makeWikiLinkCompletionsFromLine fakeNoteInfoCache (P 0 7) "[[world"
+    `shouldBe` [ wikiLinkCompletion (Range (P 0 2) (P 0 7)) (Slug "7Fu2PSiqrvz4") "Test world" "world",
+                 wikiLinkCompletion (Range (P 0 2) (P 0 7)) (Slug "kWp7rk0suUXd") "Hello world" "world",
+                 wikiLinkCompletion (Range (P 0 2) (P 0 7)) (Slug "acZlsJzsFs2g") "A wild unordinary herald" "world"
+               ]
+  it "doesn't complete when there is a complete wiki link earlier in the line but no [["
+    $ makeWikiLinkCompletionsFromLine fakeNoteInfoCache (P 0 28) "[[7Fu2PSiqrvz4|Hello]] world"
+    `shouldBe` []
+  it "is not confused by a second wikilink on the same line"
+    $ makeWikiLinkCompletionsFromLine fakeNoteInfoCache (P 0 30) "[[7Fu2PSiqrvz4|Hello]] [[Hello"
+    `shouldBe` [wikiLinkCompletion (Range (P 0 25) (P 0 30)) (Slug "kWp7rk0suUXd") "Hello world" "Hello"]
--- src/Models/NoteInfo.hs
+++ src/Models/NoteInfo.hs
@@ -23,3 +23,12 @@ instance Indexable [Slug, Title] NoteInfo where
       (ixFun (pure . Title . (.title)))
 
 type NoteInfoCache = IxSet [Slug, Title] NoteInfo
+
+fakeNoteInfoCache :: NoteInfoCache
+fakeNoteInfoCache =
+  Data.IxSet.Typed.fromList
+    [ NoteInfo (Slug "kWp7rk0suUXd") "Hello world",
+      NoteInfo (Slug "7Fu2PSiqrvz4") "Test world",
+      NoteInfo (Slug "JQiVd3GmGPpP") "Some string",
+      NoteInfo (Slug "acZlsJzsFs2g") "A wild unordinary herald"
+    ]
